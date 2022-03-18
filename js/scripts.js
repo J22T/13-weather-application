c3e345edca69a5aa554f10fd28a043ddb8e82eec
J22T <jordan22taylor@gmail.com>
2022-03-17 21:31:06 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,21 @@ fetch (apiURL)
 
 const apiURL = "//https://api.openweathermap.org/data/2.5/weather?id=5410902&appid=f02e792d64350e7f05c1bb65d77dd55c&units=imperial"
 
+const mydate = new Date();
+
+const y = mydate.getDay();
+console.log(y);5
+
+
+
+const myweekday = new Array(7);
+
+myweekday[0] = "Sunday";
+myweekday[1] = "Monday";
+myweekday[2] = "Tuesday";
+myweekday[3] = "Wednesday";
+myweekday[4] = "Thursday";
+myweekday[5] = "Friday";
+myweekday[6] = "Saturday";
+
+console.log(myweekday[3]);
