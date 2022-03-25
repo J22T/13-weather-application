e202ca05027a85fbb0427a4b1638723f8a91438d
J22T <jordan22taylor@gmail.com>
2022-03-24 23:40:51 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,48 @@
 const apiURL = "//https://api.openweathermap.org/data/2.5/weather?id=5410902&appid=f02e792d64350e7f05c1bb65d77dd55c&units=imperial"
 
 fetch (apiURL)
-.then((response)) => resonse.json())
-.then((weatherInfo) => {
-    console.log(weatherInfo);
+    .then((response)) => resonse.json())
+    .then((weatherInfo) => {
+        console.log(weatherInfo);
+
+        document.getElementById('place').innerHTML=weatherInfo.name;
 }); // end of .then
 
-fetch ("testdata.json")
-.then((mydata) => mydata.json())
-.then((mydata) => {
-    console.log(mydata);
-    console.log(mydata.url);
+// fetch ("testdata.json")
+// .then((mydata) => mydata.json())
+// .then((mydata) => {
+//     console.log(mydata);
+//     console.log(mydata.url);
 
-    document.getElementById("Primary").innerHTML = mydata.url;
+//     document.getElementById("Primary").innerHTML = mydata.url;
 
-    let sample = document.createElement("h1");
-    sample.textContent = mydata.url;
-    document.getElementById("Primary").appendChild(sample);
-});
-
-
+//     let sample = document.createElement("h1");
+//     sample.textContent = mydata.url;
+//     document.getElementById("Primary").appendChild(sample);
+// });
 
 
 
 
 
 
-const mydate = new Date();
-
-const y = mydate.getDay();
-console.log(y);5
 
 
+// const mydate = new Date();
 
-const myweekday = new Array(7);
+// const y = mydate.getDay();
+// console.log(y);5
 
-myweekday[0] = "Sunday";
-myweekday[1] = "Monday";
-myweekday[2] = "Tuesday";
-myweekday[3] = "Wednesday";
-myweekday[4] = "Thursday";
-myweekday[5] = "Friday";
-myweekday[6] = "Saturday";
 
-console.log(myweekday[3]);
+
+// const myweekday = new Array(7);
+
+// myweekday[0] = "Sunday";
+// myweekday[1] = "Monday";
+// myweekday[2] = "Tuesday";
+// myweekday[3] = "Wednesday";
+// myweekday[4] = "Thursday";
+// myweekday[5] = "Friday";
+// myweekday[6] = "Saturday";
+
+// console.log(myweekday[3]);
